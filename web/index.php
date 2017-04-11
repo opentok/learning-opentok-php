d24e12e6a939818618a1917ec190ec394a4e9717
Jeff Swartz <swartz@tokbox.com>
2017-04-11 13:20:34 -0700

    Fix issue #5 -- crash trying to render view.php.

--- web/index.php
+++ web/index.php
@@ -30,7 +30,7 @@ if (!(getenv('API_KEY') && getenv('API_SECRET'))) {
 // Instantiate a Slim app
 $app = new Slim(array(
     'log.enabled' => true,
-    'templates.path' => '../templates'
+    'templates.path' => __DIR__.'/../templates'
 ));
 
 // Intialize storage interface wrapper, store it in a singleton

3c561751f6e7ffc558a16e8311e853e5f2bcb369
Lucas Huang <lucas@tokbox.com>
2017-05-18 11:26:33 -0700

    fix a crop issue

--- web/index.php
+++ web/index.php
@@ -172,23 +172,9 @@ $app->get('/archive/:archiveId', 'cors', function ($archiveId) use ($app) {
 
 // Enable CORS functionality
 function cors() {
-    // Allow from any origin
-    if (isset( $_SERVER['HTTP_ORIGIN'])) {
-        header( "Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}" );
-        header( 'Access-Control-Allow-Credentials: true' );
-        header( 'Access-Control-Max-Age: 86400' );    // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    }
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 }
 
 // return HTTP 200 for HTTP OPTIONS requests
